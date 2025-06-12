76c6e27aef671489735704b96d109602f50d6822
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-06-12 12:40:32 +0600

    dragndrop UI added

--- src/components/DragnDropComp.jsx
+++ src/components/DragnDropComp.jsx
@@ -1,7 +1,28 @@
-
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
 
 export default function DragnDropComp() {
   return (
-    <div>Hello World! Iam DragnDropComp</div>
+    <>
+      <DndProvider backend={HTML5Backend}>
+        <div>
+          <div>
+            <h2>Drag & drop Board</h2>
+            <from>
+              <input type="text" placeholder="Name" value={0} onChange={0} />
+              <select value={0} onChange={0}>{/* array mapping*/}</select>
+              <button type="submit">Add Task</button>
+            </from>
+            {/* grid */}
+            <div>
+              <div className="grid">
+                {/* Column component mapping */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </DndProvider>
+    </>
   )
 }
