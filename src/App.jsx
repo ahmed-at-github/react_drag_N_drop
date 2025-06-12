24216beaf9dcca9ef28a62fccd31ce0b8097e268
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-06-12 12:25:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import DragnDropComp from './components/DragnDropComp'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h1>Hello World! Iam App</h1>
+     <DragnDropComp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/DragnDropComp.jsx
@@ -0,0 +1,7 @@
+
+
+export default function DragnDropComp() {
+  return (
+    <div>Hello World! Iam DragnDropComp</div>
+  )
+}
